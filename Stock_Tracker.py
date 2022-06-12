ee8c22c66431207a3009fe1310cd084fb868f68b
blake-dreyergit config --get user.name <BlakeDreyerDev@gmail.com>
2022-06-12 11:02:44 -0400

    Created Stock Tracker
    Learned to use lists

---
+++ Stock_Tracker.py
@@ -0,0 +1,13 @@
+apple_stocks = [298.18, 304.18, 289.23]
+
+apple_stocks[0] = 310
+print("Latest Value: ")
+print(apple_stocks[0])
+
+apple_stocks[1] = 310
+print("Highest Value: ")
+print(apple_stocks[1])
+
+print(f"Lowest Value: {apple_stocks[2]}")
+
+
