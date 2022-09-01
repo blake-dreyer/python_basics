4f3be4527b71912fea461290af43086958d44f7a
blake-dreyer <blakedreyerdev@gmail.com>
2022-08-31 22:03:27 -0400

    modified parrot

--- parrot.py
+++ parrot.py
@@ -1,13 +1,26 @@
 #Learning the input function
 
-#Parrot
-message = input('Tell me something and I will repeat it back to you: ')
-print(message)
+#Parrot function, ends using break
+def Parrot():
+    prompt = "\nTell me something and I will say it back to you,"
+    second = " \nor enter quit: "
+    combined = prompt+second
+    message = ''
+    while True:
+        message = input(combined)
+        if message != 'quit':
+            print (message)
+        else:
+            print ("The parrot flew away!")
+            break
+        
+        
 
 #Even or Odd
-number = input('Give me a number: ')
-number = int(number)
-if number % 2 == 0:
-    print(f'{number} is Even!\n')
-else:
-    print(f'{number} is Odd!\n')
+def Even_or_odd():
+    number = input('Give me a number: ')
+    number = int(number)
+    if number % 2 == 0:
+        print(f'{number} is Even!\n')
+    else:
+        print(f'{number} is Odd!\n')
